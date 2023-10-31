1a3a3cee769295d20cb9e677c1868c918fcfffeb
ruhulbdgit <91396640+ruhulbdgit@users.noreply.github.com>
2023-10-31 08:12:15 +0600

    testing

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head() ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <div class="container">
+        <header class="site-header">
+            <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+            <h4><?php bloginfo('description'); ?></h4>
+            <nav class="navigation-menu">
+                <?php $args = ['theme_location' => 'primary']; ?>
+                <?php wp_nav_menu($args) ?>
+            </nav>
+        </header>
